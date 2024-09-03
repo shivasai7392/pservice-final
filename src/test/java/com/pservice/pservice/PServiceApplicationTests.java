88ba24e3cde3ab8be44781aae94d061a0ad63071
shivasai7392 <shivasai7392@gmail.com>
2024-09-03 22:19:11 +0530

    Initial Setup

---
+++ src/test/java/com/pservice/pservice/PServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pservice.pservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
