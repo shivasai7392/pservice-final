63e7f2a87063327032f93eb662de79a84d2318fe
shivasai7392 <shivasai7392@gmail.com>
2024-09-17 23:18:13 +0530

    UUID generation for primary key Id

--- src/main/java/com/pservice/pservice/models/BaseModel.java
+++ src/main/java/com/pservice/pservice/models/BaseModel.java
@@ -1,9 +1,12 @@
 package com.pservice.pservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
@@ -12,5 +15,8 @@ import java.util.UUID;
 @MappedSuperclass
 public class BaseModel {
     @Id
+    @GeneratedValue(generator = "generator_name")
+    @GenericGenerator(name = "generator_name", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "binary(16)")
     private UUID id;
 }
